209cbeb35cc2783d7252c0e56628296943839775
Husni <service.has.solution@gmail.com>
2022-09-15 21:05:53 +0700

    17. Slices and  For Loops

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 	// card := "Ace of Spades"
-	card := newCard()
+	cards := []string{newCard(), "Ace of Diamonds"}
+	// append do not return modified slice but return new copy of slice
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
