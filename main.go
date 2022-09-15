410b689005f174da1fc1a05e7a45cb01f1d579f8
Husni <service.has.solution@gmail.com>
2022-09-15 20:14:49 +0700

    15. Variable Declarations

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Hi There!")
+	// `var card string = "Ace of Spades"`` is equal to `card := "Ace of Spades"``
+	card := "Ace of Spades"   // := is only use to define/intialize new variable
+	card = "Five of Diamonds" // to update just use = only
+
+	fmt.Println(card)
 }
