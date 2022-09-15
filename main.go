34f180d48faa510007954e9a4a05f90b25b45371
Husni <service.has.solution@gmail.com>
2022-09-15 20:22:07 +0700

    16. Functions and Return Types

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	// `var card string = "Ace of Spades"`` is equal to `card := "Ace of Spades"``
-	card := "Ace of Spades"   // := is only use to define/intialize new variable
-	card = "Five of Diamonds" // to update just use = only
+	// card := "Ace of Spades"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
