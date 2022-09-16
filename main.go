8d39a3f5fd7b35d02755bd53e281987b41f103e8
Husni <service.has.solution@gmail.com>
2022-09-16 20:40:08 +0700

    38. Defining Structs

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import "fmt"
+type person struct {
+	firstName string
+	lastName  string
+}
 
 func main() {
-	integers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for _, integer := range integers {
-		if integer%2 == 0 {
-			fmt.Println(integer, " is even")
-		} else {
-			fmt.Println(integer, " is odd")
-		}
-	}
 }
