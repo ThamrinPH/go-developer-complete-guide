1d2422e6c0939d98af21717bf3c45eee67970a64
Husni <service.has.solution@gmail.com>
2022-09-16 20:45:16 +0700

    30. Declaring Structs

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
+	age       int
 }
 
 func main() {
-
+	personA := person{firstName: "Fichri", lastName: "Ardianto", age: 25}
+	fmt.Println(personA, personA.firstName)
 }
