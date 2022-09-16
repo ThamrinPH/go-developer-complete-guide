f52359689a9291ab2aa12ac9fa6137072019b3ca
Husni <service.has.solution@gmail.com>
2022-09-16 20:13:28 +0700

    Even and Odd

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeckFromFile("my_cards_deck")
-	shuffledCards := cards.shuffle()
-	shuffledCards.print()
+	integers := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, integer := range integers {
+		if integer%2 == 0 {
+			fmt.Println(integer, " is even")
+		} else {
+			fmt.Println(integer, " is odd")
+		}
+	}
 }
