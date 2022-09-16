b22ebe57978b95e2839e9d841c923d134c2d737b
Husni <service.has.solution@gmail.com>
2022-09-16 20:51:57 +0700

    40. Update Struct Values

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import "fmt"
 type person struct {
 	firstName string
 	lastName  string
-	age       int
 }
 
 func main() {
-	personA := person{firstName: "Fichri", lastName: "Ardianto", age: 25}
-	fmt.Println(personA, personA.firstName)
+	var personA person
+
+	personA.firstName = "Alex"
+	personA.lastName = "Anderson"
+
+	fmt.Printf("%+v", personA)
 }
