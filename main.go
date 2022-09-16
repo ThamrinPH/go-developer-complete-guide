019cb26e9126299408720b6b57bed42d9a7eb996
Husni <service.has.solution@gmail.com>
2022-09-16 20:56:02 +0700

    41.  Embedding Structs

--- main.go
+++ main.go
@@ -2,16 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	var personA person
-
-	personA.firstName = "Alex"
-	personA.lastName = "Anderson"
+	personA := person{
+		firstName: "Jim",
+		lastName:  "Part",
+		contact: contactInfo{
+			email:   "jiim@gmail.com",
+			zipCode: 602148,
+		},
+	}
 
 	fmt.Printf("%+v", personA)
 }
