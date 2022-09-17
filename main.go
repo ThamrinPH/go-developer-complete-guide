3df0283eb060a3b2d2440206ded1038616ba05c9
Husni <service.has.solution@gmail.com>
2022-09-17 10:47:06 +0700

    50. Manipulatin Maps

--- main.go
+++ main.go
@@ -3,10 +3,29 @@ package main
 import "fmt"
 
 func main() {
+	// cara inisialisasi variable bertipe map ada beberapa cara
+	// #1
+	// var colors map[string]string -> menghasilnya map kosong
+	// #2
+	// colors := make(map[string]string) -> menghasilnya map kosong
+
+	// colors := make(map[string]string)
+	// colors["white"] = "#ffffff"
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#ff4582",
 	}
+
+	for key, val := range colors {
+		fmt.Println(key, val)
+	}
+
+	// add a key to a map
+	colors["white"] = "#ffffff"
+
+	// delete a key from a map
+	delete(colors, "red")
 
 	fmt.Println(colors)
 }
