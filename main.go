56b3f03cb58f9b2b406bec42d71609a20a513b3d
Husni <service.has.solution@gmail.com>
2022-09-17 10:40:05 +0700

    49. What's a Map?

--- main.go
+++ main.go
@@ -2,36 +2,11 @@ package main
 
 import "fmt"
 
-type contactInfo struct {
-	email   string
-	zipCode int
-}
-
-type person struct {
-	firstName string
-	lastName  string
-	contactInfo
-}
-
 func main() {
-	personA := person{
-		firstName: "Jim",
-		lastName:  "Part",
-		contactInfo: contactInfo{
-			email:   "jiim@gmail.com",
-			zipCode: 602148,
-		},
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#ff4582",
 	}
 
-	personA.updateName("Oscar")
-	personA.print()
-
-}
-
-func (p person) print() {
-	fmt.Printf("%+v", p)
-}
-
-func (pointerToPerson *person) updateName(newFirstName string) {
-	(*pointerToPerson).firstName = newFirstName
+	fmt.Println(colors)
 }
