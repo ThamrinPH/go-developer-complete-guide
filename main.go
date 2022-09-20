a0b4a2a602a71bb5f265c002c08eec6873b7816f
Husni <service.has.solution@gmail.com>
2022-09-20 20:38:20 +0700

    69. Website Status Checker

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	links := []string{
+		"https://www.google.com/",
+		"https://www.facebook.com/",
+		"https://www.stackoverflow.com/",
+		"https://www.golang.org/",
+		"https://www.amazon.com/",
+		"https://www.tokopedia.com/",
+		"https://www.shopee.com/",
+	}
+
+	for _, link := range links {
+		resp, err := http.Get(link)
+
+		if err != nil {
+			fmt.Println("Error :", err)
+		}
+
+		fmt.Println(resp)
+	}
 }
