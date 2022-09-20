161265e474a6369adccd30dfb8c3f109b88bde51
Husni <service.has.solution@gmail.com>
2022-09-20 20:32:16 +0700

    assignment 3

--- main.go
+++ main.go
@@ -3,28 +3,36 @@ package main
 import (
 	"fmt"
 	"io"
-	"net/http"
 	"os"
 )
 
-type logWriter struct{}
+/*
+Create a program that reads the contents of a text file then prints
+its contents to the terminal.
 
+The file to open should be provided as an argument to the program
+when it is executed at the terminal. For example, 'go run main.go myfile.txt'
+should open up the myfile.txt file
+
+to read in the arguments provided to a program,
+you can reference the variable 'os.Args', which is a slice of type string
+
+To open a file, check out the documentation for the "Open" function
+in the "os" package - https://golang.org/pkg/os/#Open
+
+What interfaces does the "File" type implement?
+
+if the "File" type implements the "Reader" interface, you might be able
+to reuse the io.Copy function
+*/
 func main() {
-	resp, err := http.Get("https://www.google.com/")
+	filePath := os.Args[1]
+	data, err := os.Open(filePath)
 
 	if err != nil {
-		fmt.Println("Errpr:", err)
+		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
 
-	lw := logWriter{}
-
-	io.Copy(lw, resp.Body)
-}
-
-func (logWriter) Write(bs []byte) (int, error) {
-	fmt.Println(string(bs))
-	fmt.Println("just wrote this many bytes:", len(bs))
-
-	return len(bs), nil
+	io.Copy(os.Stdout, data)
 }
