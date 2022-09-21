941558b5a03e3d280bd9275bba584f5ab54eae81
Husni <service.has.solution@gmail.com>
2022-09-21 09:58:03 +0700

    75. Channel Implementation

--- main.go
+++ main.go
@@ -13,21 +13,32 @@ func main() {
 		"https://www.golang.org/",
 		"https://www.amazon.com/",
 		"https://www.tokopedia.com/",
+		"https://www.bukalapak.com/",
 		"https://www.shopee.co.id/",
+		"https://www.olx.co.id/",
+		"https://id.yahoo.com/",
+		"https://www.bumble.com/",
 	}
+
+	c := make(chan string)
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<-c)
+
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "Yep its up"
 }
